c75e9bcaab3071113772493943c6554c83fe70c5
Arjun-Shrivas <arjunshrivas1997@gmail.com>
2022-08-23 11:28:13 +0530

    first init commit

---
+++ utils/nftport/uploadFiles.js
@@ -0,0 +1,121 @@
+const FormData = require("form-data");
+require('dotenv').config()
+const fetch = require("node-fetch");
+const path = require("path");
+const basePath = process.cwd();
+const fs = require("fs");
+const buildDir = `${basePath}/build`;
+const {
+    baseUri,
+    description,
+    extraMetadata,
+    text,
+    namePrefix
+   } = require(`${basePath}/src/config.js`);
+
+   const AUTH = process.env.AUTH;
+const TIMEOUT = 1000; // Milliseconds. Extend this if needed to wait for each upload. 1000 = 1 second.
+
+const allMetadata = [];
+
+async function main() {
+
+  if (fs.readdirSync(`${basePath}/build/images`).length > 0){
+    const files = fs.readdirSync(`${basePath}/build/images`);
+    files.sort(function(a, b){
+      return a.split(".")[0] - b.split(".")[0];
+    });
+    for (const file of files) {
+      const fileName = path.parse(file).name;
+      
+      
+      const response = await fetchWithRetry(file);
+  
+          let dateTime = Date.now();
+          let metaData = {
+          name: `${namePrefix} #${fileName}`,
+          description: description,
+          file_url: response.ipfs_url,
+          custom_fields: {
+              edition: fileName,
+              date: dateTime,
+              compiler: "Delixus inc",
+          },
+          ...extraMetadata,
+          };
+      
+    
+        fs.writeFileSync(
+          `${basePath}/build/json/${fileName}.json`,
+          JSON.stringify(metaData, null, 2)
+        );
+      
+      console.log(`${response.file_name} uploaded & ${fileName}.json Created!`);
+      
+       allMetadata.push(metaData);
+    }
+    fs.writeFileSync(
+      `${basePath}/build/json/_metadata.json`,
+      JSON.stringify(allMetadata, null, 2)
+    );
+  }else{
+     console.log("there is no Images..! put your NFT image inside build/images folder and retry again (: ")
+  }
+}
+
+main();
+
+function timer(ms) {
+  return new Promise(res => setTimeout(res, ms));
+}
+
+async function fetchWithRetry(file)  {
+  await timer(TIMEOUT)
+  return new Promise((resolve, reject) => {
+    const fetch_retry = (_file) => {
+      const formData = new FormData();
+      const fileStream = fs.createReadStream(`${basePath}/build/images/${_file}`);
+      formData.append("file", fileStream);
+
+      let url = "https://api.nftport.xyz/v0/files";
+      let options = {
+        method: "POST",
+        headers: {
+          Authorization: AUTH,
+        },
+        body: formData,
+      };
+
+      return fetch(url, options).then(async (res) => {
+          const status = res.status;
+
+          if(status === 200) {
+            return res.json();
+          }            
+          else {
+            console.error(`ERROR STATUS: ${status}`)
+            console.log('Retrying')
+            await timer(TIMEOUT)
+            fetch_retry(_file)
+          }            
+      })
+      .then(async (json) => {
+        if(json.response === "OK"){
+          return resolve(json);
+        } else {
+          console.error(`NOK: ${json.error}`)
+          console.log('Retrying')
+          await timer(TIMEOUT)
+          fetch_retry(_file)
+        }
+      })
+      .catch(async (error) => {  
+        console.error(`CATCH ERROR: ${error}`)  
+        console.log('Retrying')    
+        await timer(TIMEOUT)    
+        fetch_retry(_file)
+      });
+    }        
+    return fetch_retry(file);
+  });
+}
